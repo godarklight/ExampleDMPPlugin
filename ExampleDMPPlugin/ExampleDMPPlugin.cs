fd79c1e47fa7da532c836c9872942e07d2d30833
--global <joshblake@gmail.com>
2014-07-08 05:16:37 -0400

    Updated to use IDMPPlugin. Updated .gitignore.

--- ExampleDMPPlugin/ExampleDMPPlugin.cs
+++ ExampleDMPPlugin/ExampleDMPPlugin.cs
@@ -5,8 +5,7 @@ using ExamplePluginLogic;
 
 namespace ExampleDMPPlugin
 {
-    [DMPPlugin]
-    public class ExampleDMPPlugin
+    public class ExampleDMPPlugin : DMPPlugin
     {
         private int updateCallCount = 0;
 
@@ -17,7 +16,6 @@ namespace ExampleDMPPlugin
             int numberb = 7;
             ExamplePluginLogicMain eplm = new ExamplePluginLogicMain();
             DarkLog.Debug("Doing something magic with the numbers " + numbera + " and " + numberb + ", the result was " + eplm.DoSomethingMagicWithTwoNumbers(numbera, numberb));
-
         }
 
         public void Update()
@@ -25,22 +23,22 @@ namespace ExampleDMPPlugin
             updateCallCount++;
         }
 
-        public void OnClientConnect(ClientObject client)
+        public override void OnClientConnect(ClientObject client)
         {
             DarkLog.Debug("[ExamplePluginTest] somebody connected with the server");
         }
 
-        public void OnClientAuthenticated(ClientObject client)
+        public override void OnClientAuthenticated(ClientObject client)
         {
             DarkLog.Debug("[ExamplePluginTest] " + client.playerName + " authenticated with the server");
         }
 
-        public void OnClientDisconnect(ClientObject client)
+        public override void OnClientDisconnect(ClientObject client)
         {
             DarkLog.Debug("[ExamplePluginTest] " + client.playerName + " disconnected from the server");
         }
 
-        public void OnMessageReceived(ClientObject client, ClientMessage message)
+        public override void OnMessageReceived(ClientObject client, ClientMessage message)
         {
             DarkLog.Debug("[ExamplePluginTest] " + "Got a " + message.type.ToString() + " message from " + client.playerName);
         }
